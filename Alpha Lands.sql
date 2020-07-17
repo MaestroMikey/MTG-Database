f95ad4cdcd5b88cc75b414cea6427f6b0b9174f6
MaestroMikey <68441647+MaestroMikey@users.noreply.github.com>
2020-07-17 14:28:38 -0500

    Add files via upload

---
+++ Alpha Lands.sql
@@ -0,0 +1,20 @@
+insert into Lands (ID, Land_Name, Set_Name, Rarity, Legendary)
+values (6, 'Badlands', 'Alpha', 'Rare', 'No');
+insert into Lands (ID, Land_Name, Set_Name, Rarity, Legendary)
+values (7, 'Badlands', 'Alpha', 'Rare', 'No');
+insert into Lands (ID, Land_Name, Set_Name, Rarity, Legendary)
+values (8, 'Bayou', 'Alpha', 'Rare', 'No');
+insert into Lands (ID, Land_Name, Set_Name, Rarity, Legendary)
+values (9, 'Plateau', 'Alpha', 'Rare', 'No');
+insert into Lands (ID, Land_Name, Set_Name, Rarity, Legendary)
+values (10, 'Savannah', 'Alpha', 'Rare', 'No');
+insert into Lands (ID, Land_Name, Set_Name, Rarity, Legendary)
+values (11, 'Scrubland', 'Alpha', 'Rare', 'No');
+insert into Lands (ID, Land_Name, Set_Name, Rarity, Legendary)
+values (12, 'Taiga', 'Alpha', 'Rare', 'No');
+insert into Lands (ID, Land_Name, Set_Name, Rarity, Legendary)
+values (13, 'Tropical Island', 'Alpha', 'Rare', 'No');
+insert into Lands (ID, Land_Name, Set_Name, Rarity, Legendary)
+values (14, 'Tundra', 'Alpha', 'Rare', 'No');
+insert into Lands (ID, Land_Name, Set_Name, Rarity, Legendary)
+values (15, 'Underground Sea', 'Alpha', 'Rare', 'No');
---
+++ Update Dual Lands.sql
@@ -0,0 +1,150 @@
+update lands
+set Produces_W = 'No'
+where id = 6;
+update lands
+set Produces_U = 'No'
+where id = 6;
+update lands
+set Produces_B = 'Yes'
+where id = 6;
+update lands
+set Produces_R = 'Yes'
+where id = 6;
+update lands
+set Produces_G = 'No'
+where id = 6;
+update lands
+set Produces_W = 'No'
+where id = 7;
+update lands
+set Produces_U = 'No'
+where id = 7;
+update lands
+set Produces_B = 'Yes'
+where id = 7;
+update lands
+set Produces_R = 'No'
+where id = 7;
+update lands
+set Produces_G = 'Yes'
+where id = 7;
+update lands
+set Produces_W = 'Yes'
+where id = 8;
+update lands
+set Produces_U = 'No'
+where id = 8;
+update lands
+set Produces_B = 'No'
+where id = 8;
+update lands
+set Produces_R = 'Yes'
+where id = 8;
+update lands
+set Produces_G = 'No'
+where id = 8;
+update lands
+set Produces_W = 'Yes'
+where id = 9;
+update lands
+set Produces_U = 'No'
+where id = 9;
+update lands
+set Produces_B = 'No'
+where id = 9;
+update lands
+set Produces_R = 'No'
+where id = 9;
+update lands
+set Produces_G = 'Yes'
+where id = 9;
+update lands
+set Produces_W = 'Yes'
+where id = 10;
+update lands
+set Produces_U = 'No'
+where id = 10;
+update lands
+set Produces_B = 'Yes'
+where id = 10;
+update lands
+set Produces_R = 'No'
+where id = 10;
+update lands
+set Produces_G = 'No'
+where id = 10;
+update lands
+set Produces_W = 'No'
+where id = 11;
+update lands
+set Produces_U = 'No'
+where id = 11;
+update lands
+set Produces_B = 'No'
+where id = 11;
+update lands
+set Produces_R = 'Yes'
+where id = 11;
+update lands
+set Produces_G = 'Yes'
+where id = 11;
+update lands
+set Produces_W = 'No'
+where id = 12;
+update lands
+set Produces_U = 'Yes'
+where id = 12;
+update lands
+set Produces_B = 'No'
+where id = 12;
+update lands
+set Produces_R = 'No'
+where id = 12;
+update lands
+set Produces_G = 'Yes'
+where id = 12;
+update lands
+set Produces_W = 'Yes'
+where id = 13;
+update lands
+set Produces_U = 'Yes'
+where id = 13;
+update lands
+set Produces_B = 'No'
+where id = 13;
+update lands
+set Produces_R = 'No'
+where id = 13;
+update lands
+set Produces_G = 'No'
+where id = 13;
+update lands
+set Produces_W = 'No'
+where id = 14;
+update lands
+set Produces_U = 'Yes'
+where id = 14;
+update lands
+set Produces_B = 'Yes'
+where id = 14;
+update lands
+set Produces_R = 'No'
+where id = 14;
+update lands
+set Produces_G = 'No'
+where id = 14;
+update lands
+set Produces_W = 'No'
+where id = 15;
+update lands
+set Produces_U = 'Yes'
+where id = 15;
+update lands
+set Produces_B = 'No'
+where id = 15;
+update lands
+set Produces_R = 'Yes'
+where id = 15;
+update lands
+set Produces_G = 'No'
+where id = 15;
